90caf1cead5ee2885ff343f1bd54ff20625461c9
C1nDeRainBo0M <91606748+C1nDeRainBo0M@users.noreply.github.com>
2023-02-24 15:52:40 +0800

    Add files via upload

---
+++ AGCA.py
@@ -0,0 +1,38 @@
+import torch
+import torch.nn as nn
+from torch.nn import init
+
+
+class AGCA(nn.Module):
+    def __init__(self, in_channel, ratio):
+        super(AGCA, self).__init__()
+        hide_channel = in_channel // ratio
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)
+        self.conv1 = nn.Conv2d(in_channel, hide_channel, kernel_size=1, bias=False)
+        self.softmax = nn.Softmax(2)
+        # A0 needs to be placed on the GPU
+        self.A0 = torch.eye(hide_channel).to('cuda')
+        # A1 is initialized to 1e-6
+        self.A1 = nn.Parameter(torch.FloatTensor(torch.zeros((hide_channel, hide_channel))), requires_grad=True)
+        init.constant_(self.A1, 1e-6)
+        self.conv2 = nn.Conv1d(1, 1, kernel_size=1, bias=False)
+        self.conv3 = nn.Conv1d(1, 1, kernel_size=1, bias=False)
+        self.relu = nn.ReLU(inplace=True)
+        self.conv4 = nn.Conv2d(hide_channel, in_channel, kernel_size=1, bias=False)
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        y = self.avg_pool(x)
+        y = self.conv1(y)
+        B, C, _, _ = y.size()
+        y = y.flatten(2).transpose(1, 2)
+        A2 = self.softmax(self.conv2(y))
+        A2 = A2.expand(B, C, C)
+        A = (self.A0 * A2) + self.A1
+        y = torch.matmul(y, A)
+        y = self.relu(self.conv3(y))
+        y = y.transpose(1, 2).view(-1, C, 1, 1)
+        y = self.sigmoid(self.conv4(y))
+
+        return x * y
+    
